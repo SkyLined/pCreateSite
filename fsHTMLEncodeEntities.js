28233399b32adf4938852db150a63fb09fe4d136
SkyLined <berendjanwever@gmail.com>
2016-11-10 15:41:57 +0100

    Code cleanup

--- fsHTMLEncodeEntities.js
+++ fsHTMLEncodeEntities.js
@@ -1,6 +1,12 @@
 module.exports = fsHTMLEncodeEntities;
 
-function fsHTMLEncodeEntities(sData, bPreserveSpaces) {
-  // Minimal implementation.
-  return sData.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/ /g, bPreserveSpaces ? "&nbsp;" : " ");
-}
+function fsHTMLEncodeEntities(sText, bPreserveSpaces) {
+  return (
+    sText
+      .replace(/&/g, "&amp;")
+      .replace(/\"/g, "&quot;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/ /g, bPreserveSpaces ? "&nbsp;" : " ")
+  );
+};
