d7951e943f09c79f105274c5729ee57dfa43bb0d
SkyLined <berendjanwever@gmail.com>
2016-11-10 15:42:40 +0100

    Parse more BugId report formats

--- fReadBugIdReportSectionFromFile.js
+++ fReadBugIdReportSectionFromFile.js
@@ -1,13 +1,17 @@
 module.exports = fReadBugIdReportSectionFromFile;
-var fReadFile = require("./fReadFile");
+var fReadFile = require("./fReadFile"),
+    fsHTMLDecodeEntities = require("./fsHTMLDecodeEntities.js");
 
 var // BugId has changed over the years, so there are multiple formats we need to be able to parse:
     rBugIdSynopsis = /(<table>[\s\S]+?<\/table>)/i,
-    rBugId = /<tr><td>Id:\s*&nbsp;<\/td><td><span class="Important">(.+?)<\/span><\/td><\/tr>/i,
-    rBugLocation = /<tr><td>Location:\s*&nbsp;<\/td><td><span class="Important">(.+?)<\/span><\/td><\/tr>/i
+    rBugId = /<tr><td>(?:Bug)?Id:\s*(?:&nbsp;)?<\/td><td>(?:<span class="Important">)?(?:<b>)?(.+?)(?:<\/b>)?(?:<\/span>)?<\/td><\/tr>/i,
+    rBugLocation = /<tr><td>Location:\s*(?:&nbsp;)?<\/td><td>(?:<span class="Important">)?(.+?)(?:<\/span>)?<\/td><\/tr>/i
     rBugIdSynopsisStrip = /\s+class="[^"]*"/gi,
-    rOldBugIdSynopsis = /<FIELDSET[^>]*><LEGEND[^>]*>Cdb event information<\/LEGEND><SPAN>(<PRE>[\s\S]*?<\/PRE>)/i,
-    rOldBugAndLocationId = /id:\s+(.+?)[\r\n]/i;
+    rOldBugIdSynopsis1 = /<FIELDSET[^>]*><LEGEND[^>]*>Cdb event information<\/LEGEND><SPAN>(<PRE>[\s\S]*?<\/PRE>)/i,
+    rOldBugAndLocationId1 = /id:\s+(.+?)[\r\n]/i,
+    rOldBugIdSynopsis2 = /<h1 class="Header">Details<\/h1>[ \r\n]+<div class="Content">([\s\S]*?)<\/div>/i,
+    rOldBugAndLocationId2 = /id:\s+(.+?)[\r\n]/i,
+    rOldBugIdSynopsisStrip2 = /<[^>]+>/gi;
 
 function fReadBugIdReportSectionFromFile(sSectionFilePath, dxSection, fCallback) {
   return fReadFile(sSectionFilePath, function (oError, sBugIdReportHTML) {
@@ -15,13 +19,14 @@ function fReadBugIdReportSectionFromFile(sSectionFilePath, dxSection, fCallback)
       return fCallback(oError);
     };
     var asBugIdSynopsisMatch = sBugIdReportHTML.match(rBugIdSynopsis),
-        asOldBugIdSynopsisMatch = asBugIdSynopsisMatch ? null : sBugIdReportHTML.match(rOldBugIdSynopsis);
+        asOldBugIdSynopsisMatch1 = asBugIdSynopsisMatch ? null : sBugIdReportHTML.match(rOldBugIdSynopsis1),
+        asOldBugIdSynopsisMatch2 = asBugIdSynopsisMatch || asOldBugIdSynopsisMatch1 ? null : sBugIdReportHTML.match(rOldBugIdSynopsis2);
     if (asBugIdSynopsisMatch) {
       var sBugIdSynopsisHTML = asBugIdSynopsisMatch[1];
       var asBugIdMatch = sBugIdSynopsisHTML.match(rBugId),
-          sBugId = asBugIdMatch ? asBugIdMatch[1] : null,
+          sBugId = asBugIdMatch ? fsHTMLDecodeEntities(asBugIdMatch[1]) : null,
           asBugLocationMatch = sBugIdSynopsisHTML.match(rBugLocation),
-          sBugLocation = asBugLocationMatch ? asBugLocationMatch[1] : null;
+          sBugLocation = asBugLocationMatch ? fsHTMLDecodeEntities(asBugLocationMatch[1]) : null;
       if (!sBugId) {
         return fCallback(new Error("BugId report id does not appear to have a known format in " + sSectionFilePath));
       };
@@ -31,12 +36,21 @@ function fReadBugIdReportSectionFromFile(sSectionFilePath, dxSection, fCallback)
       sType = "BugId report";
       sName = sBugId + " @ " + sBugLocation;
       sBugIdSynopsisHTML = sBugIdSynopsisHTML.replace(rBugIdSynopsisStrip, "")
-    } else if (asOldBugIdSynopsisMatch) {
-      var sBugIdSynopsisHTML = asOldBugIdSynopsisMatch[1],
-          asBugLocationAndIdMatch = sBugIdSynopsisHTML.match(rOldBugAndLocationId),
-          sBugLocationAndId = asBugLocationAndIdMatch ? asBugLocationAndIdMatch[1] : null;
+    } else if (asOldBugIdSynopsisMatch1) {
+      var sBugIdSynopsisHTML = asOldBugIdSynopsisMatch1[1],
+          asBugLocationAndIdMatch = sBugIdSynopsisHTML.match(rOldBugAndLocationId1),
+          sBugLocationAndId = asBugLocationAndIdMatch ? fsHTMLDecodeEntities(asBugLocationAndIdMatch[1]) : null;
       if (!sBugLocationAndId) {
-        return fCallback(new Error("Old BugId report id does not appear to have a known format in " + sSectionFilePath));
+        return fCallback(new Error("BugId report id does not appear to have a known format in " + sSectionFilePath));
+      };
+      sType = "old BugId report";
+      sName = sBugLocationAndId;
+    } else if (asOldBugIdSynopsisMatch2) {
+      var sBugIdSynopsisHTML = fsHTMLDecodeEntities(asOldBugIdSynopsisMatch2[1].replace(rOldBugIdSynopsisStrip2, "")),
+          asBugLocationAndIdMatch = sBugIdSynopsisHTML.match(rOldBugAndLocationId2),
+          sBugLocationAndId = asBugLocationAndIdMatch ? fsHTMLDecodeEntities(asBugLocationAndIdMatch[1]) : null;
+      if (!sBugLocationAndId) {
+        return fCallback(new Error("BugId report id does not appear to have a known format in " + sSectionFilePath));
       };
       sType = "old BugId report";
       sName = sBugLocationAndId;
